6645d0f28b76e2926785d594dd0192da6d0bfa2f
Katrina Steinman <ksteinma@adit.mines.edu>
2016-10-23 15:14:47 -0600

    added all files

---
+++ src/clueGame/Solution.java
@@ -0,0 +1,11 @@
+package clueGame;
+public class Solution {
+	public String person = "";
+	public String room = "";
+	public String weapon = "";
+	
+	//from full deck, randomly remove one of each type
+	//removing one thing from each of the three arraylists and making that the solution 
+	//declare newDeck to contain all remaining cards
+	//randomly distribute these cards to each player's private deck
+}
