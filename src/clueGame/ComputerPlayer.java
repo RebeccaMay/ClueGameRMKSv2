015bfa7338eb9b6572906472926025808b32b166
Katrina Steinman <ksteinma@adit.mines.edu>
2016-10-25 15:52:47 -0600

    test target locations failing

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,10 +1,11 @@
 package clueGame;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class ComputerPlayer extends Player {
 	private BoardCell lastRoomVisited = new BoardCell();
-	
+	private ArrayList<BoardCell> roomsVisited = new ArrayList<BoardCell>();
 	public BoardCell pickLocation(Set<BoardCell> targets){ //-------------------------------------------RETURNING TEMP
 		BoardCell temp = new BoardCell();
 		return temp;
@@ -17,4 +18,7 @@ public class ComputerPlayer extends Player {
 	//public Solution createSuggestion(){}
 	
 	//getter and setter
+	public void addRoomToRoomsVisited(BoardCell room){
+		
+	}
 }
